0bc97ea9955b90e0879350c06e85e5084b5e44eb
Frank Reimann <frank.reimann@jambit.com>
2023-05-30 19:02:22 +0200

    check if user has subscription active

--- packages/payment-module/src/routes/stripe/customer-products/+server.ts
+++ packages/payment-module/src/routes/stripe/customer-products/+server.ts
@@ -10,25 +10,38 @@ export const GET = async (event: RequestEvent) => {
 	if (!email) return json({ message: 'email not defined' }, { status: 400 });
 
 	try {
-		const res = await getProductsAndSubscriptionsUserPaidFor(email);
+		const res = await getActiveSubscriptionsUser(email);
 
-		return json(res);
+		return json({ subscriptions: res });
 	} catch (e: any) {
 		return json({ message: e.message }, { status: 400 });
 	}
 };
 
-async function getProductsAndSubscriptionsUserPaidFor(email: string) {
+async function getActiveSubscriptionsUser(email: string) {
 	const customer = 'cus_NzOhN7bd0k9xoW'; // FIXME remote static customer
-	const paymentIntents = await stripe.subscriptions.search({
-		query: `status:'active'`
+	const paymentIntents = await stripe.subscriptions.list({
+		limit: 100,
+		customer,
+		status: 'active'
 	});
 
-	const res = paymentIntents.data.map((it) => ({ product: it }));
+	// FIXME remote static stuff
+	const subscriptionProductIds = { basic: 'prod_NzNXW6lsQeBMVf', premium: 'prod_NzNXMjIq68m5SH' };
+	const tokenProductIds = ['prod_Nz5pmL8zjZrDAP'];
+
+	const subscribedProducts = paymentIntents.data.map((it) => ({
+		product: it.plan.product
+	}));
+
+	const hasBasic =
+		subscribedProducts.filter((it) => it.product == subscriptionProductIds['basic']).length > 0;
+	const hasPremium =
+		subscribedProducts.filter((it) => it.product == subscriptionProductIds['premium']).length > 0;
 
 	if (paymentIntents.has_more)
 		console.warn(
 			'potentially more paymentIntents with the same email, this could result in unexpected behavior'
 		);
-	return res[0] ?? [];
+	return { hasPremium, hasBasic };
 }
