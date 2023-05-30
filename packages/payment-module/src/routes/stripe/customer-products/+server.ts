2d6e56463ff7c2c8bc551c3252b38fa840ca8e04
Frank Reimann <frank.reimann@jambit.com>
2023-05-30 19:22:45 +0200

    WIP

--- packages/payment-module/src/routes/stripe/customer-products/+server.ts
+++ packages/payment-module/src/routes/stripe/customer-products/+server.ts
@@ -3,6 +3,10 @@ import { json, error } from '@sveltejs/kit';
 
 import stripe from '../_stripe';
 
+// FIXME remote static stuff
+const subscriptionProductIds = { basic: 'prod_NzNXW6lsQeBMVf', premium: 'prod_NzNXMjIq68m5SH' };
+const tokenProductIds = { basicToken: 'prod_Nz5pmL8zjZrDAP' };
+
 /** @type {import('./$types').RequestHandler} */
 export const GET = async (event: RequestEvent) => {
 	const email = event.url.searchParams.get('email');
@@ -10,9 +14,11 @@ export const GET = async (event: RequestEvent) => {
 	if (!email) return json({ message: 'email not defined' }, { status: 400 });
 
 	try {
-		const res = await getActiveSubscriptionsUser(email);
+		const subscriptions = await getActiveSubscriptionsUser(email);
+		const res2 = await getBoughtProducts(email, tokenProductIds['basicToken'], '');
+		getBoughtProducts;
 
-		return json({ subscriptions: res });
+		return json({ subscriptions, res2 });
 	} catch (e: any) {
 		return json({ message: e.message }, { status: 400 });
 	}
@@ -20,17 +26,13 @@ export const GET = async (event: RequestEvent) => {
 
 async function getActiveSubscriptionsUser(email: string) {
 	const customer = 'cus_NzOhN7bd0k9xoW'; // FIXME remote static customer
-	const paymentIntents = await stripe.subscriptions.list({
+	const subscriptions = await stripe.subscriptions.list({
 		limit: 100,
 		customer,
 		status: 'active'
 	});
 
-	// FIXME remote static stuff
-	const subscriptionProductIds = { basic: 'prod_NzNXW6lsQeBMVf', premium: 'prod_NzNXMjIq68m5SH' };
-	const tokenProductIds = ['prod_Nz5pmL8zjZrDAP'];
-
-	const subscribedProducts = paymentIntents.data.map((it) => ({
+	const subscribedProducts = subscriptions.data.map((it) => ({
 		product: it.plan.product
 	}));
 
@@ -39,9 +41,24 @@ async function getActiveSubscriptionsUser(email: string) {
 	const hasPremium =
 		subscribedProducts.filter((it) => it.product == subscriptionProductIds['premium']).length > 0;
 
+	if (subscriptions.has_more)
+		console.warn(
+			'potentially more subscriptions with the same email, this could result in unexpected behavior'
+		);
+	return { hasPremium, hasBasic };
+}
+
+async function getBoughtProducts(email: string, productId: string, beforeDate: string) {
+	const customer = 'cus_NzOhN7bd0k9xoW'; // FIXME remote static customer
+	const paymentIntents = await stripe.paymentIntents.search({
+		limit: 100,
+		query: `status:'succeeded'`
+		//customer
+	});
+
 	if (paymentIntents.has_more)
 		console.warn(
 			'potentially more paymentIntents with the same email, this could result in unexpected behavior'
 		);
-	return { hasPremium, hasBasic };
+	return { paymentIntents: paymentIntents.data };
 }
