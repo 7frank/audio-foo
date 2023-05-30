e377c4b2be07f4afce1ea1b1ba9ae6b5a1d4b649
Frank Reimann <frank.reimann@jambit.com>
2023-05-30 13:37:45 +0200

    reduce clutter

--- packages/sveltekit-stripe2/src/routes/stripe/success/+page.server.ts
+++ packages/sveltekit-stripe2/src/routes/stripe/success/+page.server.ts
@@ -1,19 +1,9 @@
-// TODO
-import * as dotenv from 'dotenv';
-dotenv.config();
-
-import { env } from '$env/dynamic/private';
-import Stripe from 'stripe';
+import stripe from '../_stripe';
 
 export async function load({ url }) {
 	const sessionId = url.searchParams.get('sessionId');
 
-	if (!env.STRIPE_SECRET_KEY) throw new Error('STRIPE_SECRET_KEY undefined');
 	if (!sessionId) throw new Error('sessionId undefined');
-
-	const stripe = new Stripe(env['STRIPE_SECRET_KEY'], {
-		apiVersion: '2022-11-15'
-	});
 
 	const session = await stripe.checkout.sessions.retrieve(sessionId);
 
