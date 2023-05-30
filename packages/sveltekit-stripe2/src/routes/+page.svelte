092e5be464d4f4db991b41eedd1241490d15c384
Frank Reimann <frank.reimann@jambit.com>
2023-05-30 11:31:35 +0200

    copy files from sveletekit-stripe

--- packages/sveltekit-stripe2/src/routes/+page.svelte
+++ packages/sveltekit-stripe2/src/routes/+page.svelte
@@ -1,2 +1,50 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script context="module" lang="ts">
+	//	export const prerender = true;
+
+	export async function load({ fetch }) {
+		const res = await fetch('/plans.json');
+		let plans = [];
+		if (res.ok) plans = await res.json();
+
+		return {
+			status: 200,
+			props: {
+				plans: plans
+			}
+		};
+	}
+</script>
+
+<script lang="ts">
+	import Pricing from '$lib/Pricing/index.svelte';
+	import StripeProvider from '$lib/stripe/StripeProvider.svelte';
+
+	export let plans;
+</script>
+
+<svelte:head>
+	<title>Pricing</title>
+</svelte:head>
+
+<StripeProvider>
+	<section>
+		<h2>Choose your plan and start counting!</h2>
+		<Pricing {plans} />
+	</section>
+</StripeProvider>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 1;
+	}
+
+	h2 {
+		color: var(--heading-color);
+		font-weight: bold;
+		font-size: 2rem;
+	}
+</style>
--- packages/sveltekit-stripe2/svelte.config.js
+++ packages/sveltekit-stripe2/svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+//import cspDirectives from './csp-directives.mjs';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,6 +13,13 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
+		// hydrate the <div id="svelte"> element in src/app.html
+		// target: '#svelte'
+
+		// csp: {
+		// 	mode: 'hash',
+		// 	directives: cspDirectives
+		// }
 	}
 };
 
