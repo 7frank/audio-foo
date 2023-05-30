7b917b87c81f0dc6c1d485c7962c46febafa08a2
Frank Reimann <frank.reimann@jambit.com>
2023-05-30 16:10:06 +0200

    add zod for validation

--- packages/payment-module/src/routes/stripe/checkout-session/+server.ts
+++ packages/payment-module/src/routes/stripe/checkout-session/+server.ts
@@ -1,19 +1,27 @@
-import type { RequestEvent, RequestHandler } from '@sveltejs/kit';
+import type { RequestEvent } from '@sveltejs/kit';
 import { json, error } from '@sveltejs/kit';
 import stripe from '../_stripe';
+import { z } from 'zod';
 
-export const POST: RequestHandler = async (event: RequestEvent) => {
+const CheckoutSession = z.object({
+	priceId: z.string(),
+	customerId: z.string().optional() // FIXME if we allow the client to set the customerId we have to validate that the email of the sso user is the email of the customer
+});
+
+export type CheckoutSession = z.infer<typeof CheckoutSession>;
+
+/** @type {import('./$types').RequestHandler} */
+export const POST = async (event: RequestEvent) => {
 	const req = event.request;
 
-	const formData = await req.json();
-	const priceId = formData.priceId;
+	const formData = CheckoutSession.parse(await req.json());
 
-	if (typeof priceId !== 'string') {
-		return error(400, new Error('priceId is required'));
-	}
+	const priceId = formData.priceId;
+	const customerId = formData.customerId;
 
 	try {
 		const session = await stripe.checkout.sessions.create({
+			customer: customerId,
 			mode: 'subscription',
 			payment_method_types: ['card'],
 			line_items: [
