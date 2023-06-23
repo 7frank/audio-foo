896408bad8a498d54de3bbc0ebf9366148ec2beb
Frank Reimann <frank.reimann@jambit.com>
2023-06-24 00:47:02 +0200

    fix logic flaws

--- apps/typeracer-clone/src/routes/Counter.svelte
+++ apps/typeracer-clone/src/routes/Counter.svelte
@@ -1,24 +1,23 @@
 <script lang="ts">
 	import { createRace } from "./store";
 
-
-
-
-
 	const race= createRace("Type the text here");
-     
-
+    
 	let diffPos=0
     let currentCursorPos=0
 	let ref:HTMLInputElement;
-      
+    let succeeded=false 
+	
     function start() {
           if (!$race. isTyping) {
-              console.log("start")
+              
 			  $race.isTyping = true;
 			  $race.startTime = new Date();
 			  $race.interval = setInterval(() => run(), 50);
-			  ref.focus(); 
+			  $race.userInput = "";
+			  setTimeout(() => ref.focus(), 0);
+
+			  succeeded=false
           }
         }
       
@@ -27,7 +26,8 @@
             $race.isTyping = false;
             $race.endTime = new Date();
             clearInterval($race.interval!);
-            calculateWPM();
+            calculateWPM($race.endTime);
+			succeeded=true
           }
         }
       
@@ -35,16 +35,22 @@
           // Perform any additional logic during the typing practice interval
           // For example, you can track user input and update the displayed text.
 
-		  calculateWPM()
-		  diffPos=findFirstDifference($race.text, $race.userInput)
+		  calculateWPM(new Date())
+		  const {diffPos:_d,isSame}=findFirstDifference($race.text, $race.userInput)
+		  diffPos=_d
 		  currentCursorPos=$race.userInput.length
+
+
+		  if (isSame) {
+			stop();
+		  }
         }
       
-         function calculateWPM() {
-          const timeInSeconds = ($race.endTime.getTime() - $race.startTime.getTime()) / 1000;
+         function calculateWPM(t:Date) {
+          const timeInSeconds = (t.getTime() - $race.startTime.getTime()) / 1000;
          
           $race.wpm = Math.round(diffPos / (timeInSeconds / 5));
-          console.log(`Your WPM: ${$race.wpm}`);
+         
         }
       
 
@@ -54,18 +60,18 @@
 			
 			for (let i = 0; i < length; i++) {
 				if (text1[i] !== text2[i]) {
-				return i;
+				return {diffPos:i,isSame:false};
 				}
 			}
 			
 			// If the loop completes without finding a difference,
 			// check if the lengths of the texts are different
 			if (text1.length !== text2.length) {
-				return length;
+				return {diffPos:length,isSame:false};
 			}
 			
 			// If the lengths and all characters are the same, return -1
-			return -1;
+			return {diffPos:length, isSame:true};
 			}
 
 			
@@ -83,7 +89,9 @@
   
  
   <button on:click={()=>start()} >Start Race</button>
-
+   {#if succeeded}
+   <img src="https://www.w3schools.com/tags/smiley.gif" alt="Smiley face" height="42" width="42">
+   {/if}
   <h1>WPM: {$race.wpm} </h1>
 
   <p>	{#each $race.text.split("") as item, i (i)}
