e34b0fb5a646543a8e962cefa27dfa01364ca8fb
Frank Reimann <frank.reimann@jambit.com>
2023-06-24 18:15:44 +0200

    restructuring

--- apps/typeracer-clone/src/routes/Counter.svelte
+++ apps/typeracer-clone/src/routes/Counter.svelte
@@ -1,128 +1,122 @@
 <script lang="ts">
-	import { createRace } from "./store.svelte";
+	import { createRace } from './store.svelte';
 
-	const race= createRace("Type the text here");
-    
-	let diffPos=0
-    let currentCursorPos=0
-	let ref:HTMLInputElement;
-  
-	
-    function start() {
-		$race.countDown=3;
-		
+	const race = createRace('Type the text here');
 
-		const timerId=setInterval(()=>{
-			$race.countDown-=0.05
-			
-			if (!$race. isTyping && $race.countDown<=0) {
-              
-			  $race.isTyping = true;
-			  $race.startTime = new Date();
-			  $race.interval = setInterval(() => run(), 50);
-			  $race.userInput = "";
-			  setTimeout(() => ref.focus(), 10);
+	let ref: HTMLInputElement;
 
-			  $race.succeeded=false
+	function onCountDownTick() {
+		$race.countDown -= 0.05;
 
-			  clearInterval(timerId)
-          }
+		if (!$race.isTyping && $race.countDown <= 0) {
+			$race.isTyping = true;
+			$race.startTime = new Date();
+			$race.interval = setInterval(() => run(), 50);
+			$race.userInput = '';
 
-		},50)
-		
-		
-        }
-      
-        function stop() {
-          if ($race.isTyping) {
-            $race.isTyping = false;
-            $race.endTime = new Date();
-            clearInterval($race.interval!);
-            calculateWPM($race.endTime);
-			$race.succeeded=true
-          }
-        }
-      
-        function run() {
-          // Perform any additional logic during the typing practice interval
-          // For example, you can track user input and update the displayed text.
+			// focus fix
+			setTimeout(() => ref.focus(), 10);
 
-		  calculateWPM(new Date())
-		  const {diffPos:_d,isSame}=findFirstDifference($race.text, $race.userInput)
-		  diffPos=_d
-		  currentCursorPos=$race.userInput.length
+			$race.succeeded = false;
 
-
-		  if (isSame) {
-			stop();
-		  }
-        }
-      
-         function calculateWPM(t:Date) {
-          const timeInSeconds = (t.getTime() - $race.startTime.getTime()) / 1000;
-         
-          $race.wpm = Math.round(diffPos / (timeInSeconds / 5));
-         
-        }
-      
-
-
-		function findFirstDifference(text1:string, text2:string) {
-			const length = Math.min(text1.length, text2.length);
-			
-			for (let i = 0; i < length; i++) {
-				if (text1[i] !== text2[i]) {
-				return {diffPos:i,isSame:false};
-				}
-			}
-			
-			// If the loop completes without finding a difference,
-			// check if the lengths of the texts are different
-			if (text1.length !== text2.length) {
-				return {diffPos:length,isSame:false};
-			}
-			
-			// If the lengths and all characters are the same, return -1
-			return {diffPos:length, isSame:true};
-			}
-
-			
-  </script>
-  
-  <style>
-	input {
-	  width: 60%;
-	  
-	  resize: none;
+			clearInterval($race.countDownTimerId);
+		}
 	}
-  </style>
-  
-  <h1>Typing Practice</h1>
-  <ul>
+
+	function start() {
+		$race.countDown = 3;
+		$race.countDownTimerId = setInterval(onCountDownTick, 50);
+	}
+
+	function stop() {
+		if ($race.isTyping) {
+			$race.isTyping = false;
+			$race.endTime = new Date();
+			clearInterval($race.interval!);
+			calculateWPM($race.endTime);
+			$race.succeeded = true;
+		}
+	}
+
+	function run() {
+		// Perform any additional logic during the typing practice interval
+		// For example, you can track user input and update the displayed text.
+
+		calculateWPM(new Date());
+		const { diffPos: _d, isSame } = findFirstDifference($race.text, $race.userInput);
+		$race.diffPos = _d;
+		$race.currentCursorPos = $race.userInput.length;
+
+		if (isSame) {
+			stop();
+		}
+	}
+
+	function calculateWPM(t: Date) {
+		const timeInSeconds = (t.getTime() - $race.startTime.getTime()) / 1000;
+
+		$race.wpm = Math.round($race.diffPos / (timeInSeconds / 5));
+	}
+
+	function findFirstDifference(text1: string, text2: string) {
+		const length = Math.min(text1.length, text2.length);
+
+		for (let i = 0; i < length; i++) {
+			if (text1[i] !== text2[i]) {
+				return { diffPos: i, isSame: false };
+			}
+		}
+
+		// If the loop completes without finding a difference,
+		// check if the lengths of the texts are different
+		if (text1.length !== text2.length) {
+			return { diffPos: length, isSame: false };
+		}
+
+		// If the lengths and all characters are the same, return -1
+		return { diffPos: length, isSame: true };
+	}
+</script>
+
+<h1>Typing Practice</h1>
+<ul>
 	<li>train individual weaknesses by selecting words from a sentence after the race</li>
-	<li>re-implement the word by word feature of typeracer</li>
-	<li>implement the race track / progress bar and give it a "spin" some eye candy for sections where your speed is increasing</li>
+	<li>re-implement the word by word feature of typeRacer</li>
+	<li>
+		implement the race track / progress bar and give it a "spin" some eye candy for sections where
+		your speed is increasing
+	</li>
 	<li>try to implement other type racer features later on</li>
-  </ul>
- 
-  
-  {#if $race.countDown>0}
-  <p>Race starts in {$race.countDown}</p>
-  {:else}
-  <button on:click={()=>start()} >Start Race</button>
-  {/if}
+</ul>
 
-   {#if $race.succeeded}
-   <img src="https://www.w3schools.com/tags/smiley.gif" alt="Smiley face" height="42" width="42">
-   {/if}
-   
-  <h1>WPM: {$race.wpm} </h1>
+{#if $race.countDown > 0}
+	<p>Race starts in {$race.countDown}</p>
+{:else}
+	<button on:click={() => start()}>Start Race</button>
+{/if}
 
-  <p>	{#each $race.text.split("") as item, i (i)}
-	<span style="background-color: {i<diffPos?"lightgreen": (i<currentCursorPos && diffPos<currentCursorPos )?"red":"white"};">{item}</span>
+{#if $race.succeeded}
+	<img src="https://www.w3schools.com/tags/smiley.gif" alt="Smiley face" height="42" width="42" />
+{/if}
+
+<h1>WPM: {$race.wpm}</h1>
+
+<p>
+	{#each $race.text.split('') as item, i (i)}
+		<span
+			style="background-color: {i < $race.diffPos
+				? 'lightgreen'
+				: i < $race.currentCursorPos && $race.diffPos < $race.currentCursorPos
+				? 'red'
+				: 'white'};">{item}</span
+		>
 	{/each}
-  </p>
-  <input bind:this={ref}
-	bind:value={$race.userInput}
-	disabled={!$race.isTyping}
-  />
+</p>
+<input bind:this={ref} bind:value={$race.userInput} disabled={!$race.isTyping} />
+
+<style>
+	input {
+		width: 60%;
+		resize: none;
+	}
+</style>
--- apps/typeracer-clone/src/routes/store.svelte
+++ apps/typeracer-clone/src/routes/store.svelte
@@ -1,36 +1,38 @@
-<script lang="ts"  context="module">
-import { writable } from 'svelte/store';
+<script lang="ts" context="module">
+	import { writable } from 'svelte/store';
 
-interface Race {
-	interval: NodeJS.Timeout | null;
-	startTime: Date;
-	endTime: Date;
-	isTyping: boolean;
-	wpm: number;
-	readonly text: string;
-	userInput: string;
-	succeeded:boolean;
-    countDown:number;
-}
+	interface Race {
+		interval?: NodeJS.Timeout;
+		countDownTimerId?: NodeJS.Timeout;
+		startTime: Date;
+		endTime: Date;
+		isTyping: boolean;
+		wpm: number;
+		readonly text: string;
+		userInput: string;
+		succeeded: boolean;
+		countDown: number;
+		diffPos: number;
+		currentCursorPos: number;
+	}
 
-export function createRace(text: string) {
-	const store = writable<Race>({
-		interval: null,
-		startTime: new Date(),
-		endTime: new Date(),
-		userInput: '',
-		isTyping: false,
-		wpm: 0,
-		text: text
-			.replace(/\s+/g, ' ') // Replace consecutive whitespace with a single space
-			.replace(/\s/g, ' '), // Convert other whitespace characters to spaces
-		succeeded:false, 
+	export function createRace(text: string) {
+		const store = writable<Race>({
+			startTime: new Date(),
+			endTime: new Date(),
+			userInput: '',
+			isTyping: false,
+			wpm: 0,
+			text: text
+				.replace(/\s+/g, ' ') // Replace consecutive whitespace with a single space
+				.replace(/\s/g, ' '), // Convert other whitespace characters to spaces
+			succeeded: false,
 
-   		countDown:0
-	});
+			countDown: 0,
+			diffPos: 0,
+			currentCursorPos: 0
+		});
 
-	return store;
-}
-
-
+		return store;
+	}
 </script>
