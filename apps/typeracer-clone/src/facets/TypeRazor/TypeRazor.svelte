98eb0a50559eaab7c694c0ff5f02aef4d562c874
Frank Reimann <frank.reimann@jambit.com>
2023-06-27 00:42:44 +0200

    ui clean up

--- apps/typeracer-clone/src/facets/TypeRazor/TypeRazor.svelte
+++ apps/typeracer-clone/src/facets/TypeRazor/TypeRazor.svelte
@@ -94,6 +94,7 @@
 		</CountDown>
 	</div>
 {/if}
+
 {#if $race.status == 'started' && $race.elapsedMs < 1000}
 	<div
 		class="box"
@@ -105,69 +106,75 @@
 	</div>
 {/if}
 
-{#if $race.status == 'idle' || $race.status == 'succeeded' || $race.status == 'failed' || $race.status == 'aborted'}
-	<button class="racing" on:click={() => start()}>Start a new Race</button>
-{/if}
-
-{#if $race.status == 'succeeded'}
-	<img src="https://www.w3schools.com/tags/smiley.gif" alt="Smiley face" height="42" width="42" />
-{/if}
-
-<ProgressBar
-	isBotEnabled={false}
-	userName={'7frank'}
-	progress={($race.diffPos / $race.text.length) * 100}
-	wpm={$race.wpm}
-	isStreetAnimated={$race.status == 'started'}
-/>
-
-{#if $race.status != 'idle'}
-	<ProgressBar
-		isBotEnabled={$race.status == 'started'}
-		isStreetAnimated={$race.status == 'started'}
-	/>
-	<ProgressBar
-		isBotEnabled={$race.status == 'started'}
-		isStreetAnimated={$race.status == 'started'}
-	/>
-	<ProgressBar
-		isBotEnabled={$race.status == 'started'}
-		isStreetAnimated={$race.status == 'started'}
-	/>
-	<ProgressBar
-		isBotEnabled={$race.status == 'started'}
-		isStreetAnimated={$race.status == 'started'}
-	/>
-{/if}
-
-<p class="text">
-	{#if $race.status != 'idle'}
-		{#each $race.text.split('') as item, i (i)}
-			<span
-				style="background-color: {i < $race.diffPos
-					? 'lightgreen'
-					: i < $race.currentCursorPos && $race.diffPos < $race.currentCursorPos
-					? 'red'
-					: 'white'};">{item}</span
-			>
-		{/each}
+<div class="dialog">
+	{#if $race.status == 'idle' || $race.status == 'succeeded' || $race.status == 'failed' || $race.status == 'aborted'}
+		<button class="racing" on:click={() => start()}>Start a new Race</button>
 	{/if}
-</p>
-<input bind:this={ref} bind:value={$race.userInput} disabled={!$race.isTyping} />
+
+	{#if $race.status == 'succeeded'}
+		<img src="https://www.w3schools.com/tags/smiley.gif" alt="Smiley face" height="42" width="42" />
+	{/if}
+
+	<ProgressBar
+		isBotEnabled={false}
+		userName={'7frank'}
+		progress={($race.diffPos / $race.text.length) * 100}
+		wpm={$race.wpm}
+		isStreetAnimated={$race.status == 'started'}
+	/>
+
+	{#if $race.status != 'idle'}
+		<ProgressBar
+			isBotEnabled={$race.status == 'started'}
+			isStreetAnimated={$race.status == 'started'}
+		/>
+		<ProgressBar
+			isBotEnabled={$race.status == 'started'}
+			isStreetAnimated={$race.status == 'started'}
+		/>
+		<ProgressBar
+			isBotEnabled={$race.status == 'started'}
+			isStreetAnimated={$race.status == 'started'}
+		/>
+		<ProgressBar
+			isBotEnabled={$race.status == 'started'}
+			isStreetAnimated={$race.status == 'started'}
+		/>
+	{/if}
+
+	<p class="text">
+		{#if $race.status == 'idle'}
+			Welcome TypeRazor, let's start a new race and bring you to the top!<br />
+			Press the start button when ready.
+		{/if}
+		{#if $race.status != 'idle'}
+			{#each $race.text.split('') as item, i (i)}
+				<span
+					style="background-color: {i < $race.diffPos
+						? 'lightgreen'
+						: i < $race.currentCursorPos && $race.diffPos < $race.currentCursorPos
+						? 'red'
+						: 'white'};">{item}</span
+				>
+			{/each}
+		{/if}
+	</p>
+	<input bind:this={ref} bind:value={$race.userInput} disabled={!$race.isTyping} />
+</div>
 
 <style>
 	input {
-		width: 90%;
 		resize: none;
 		padding: 0.5em;
 		font-size: 2rem;
-		margin: 0 12.5em;
+		margin: 0 0em;
 	}
 
 	.text {
 		font-size: 2rem;
-		width: 90%;
+
 		height: auto;
+		min-height: 200px;
 
 		border: 1px solid grey;
 		padding: 1em;
@@ -187,5 +194,11 @@
 
 	button {
 		padding: 0.5em;
+	}
+
+	.dialog {
+		display: flex;
+		flex-direction: column;
+		width: 90%;
 	}
 </style>
